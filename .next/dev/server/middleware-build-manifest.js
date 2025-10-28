f092af33afa31184d22ffbf1de9175a022747fc7
hyc0812 <hyc0812@163.com>
2025-10-28 11:14:55 -0600

    bug fixed

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -23,19 +23,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/[root-of-the-server]__e3799155._.js",
       "static/chunks/Downloads_lottery-frontend_pages__app_2da965e7._.js",
       "static/chunks/turbopack-Downloads_lottery-frontend_pages__app_0703ebfa._.js"
-    ],
-    "/_error": [
-      "static/chunks/a1eeb_next_dist_compiled_0117e3a4._.js",
-      "static/chunks/a1eeb_next_dist_shared_lib_d57f6b26._.js",
-      "static/chunks/a1eeb_next_dist_client_f2856d9c._.js",
-      "static/chunks/a1eeb_next_dist_9d78c5e7._.js",
-      "static/chunks/a1eeb_next_error_61795d1e.js",
-      "static/chunks/[next]_entry_page-loader_ts_27408deb._.js",
-      "static/chunks/a1eeb_react-dom_6b8134f4._.js",
-      "static/chunks/a1eeb_e17d8d40._.js",
-      "static/chunks/[root-of-the-server]__5e635925._.js",
-      "static/chunks/Downloads_lottery-frontend_pages__error_2da965e7._.js",
-      "static/chunks/turbopack-Downloads_lottery-frontend_pages__error_cea2a338._.js"
     ]
   },
   "devFiles": [],
